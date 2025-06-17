9ff026b5de9b31b48622e748c1b8611dcf09e8db
Willian Rodrigues <willian.rsilva1994@gmail.com>
2025-06-16 23:24:35 -0300

    fix(api) - Activate AuthGuard

--- api/src/providers/auth/auth.module.ts
+++ api/src/providers/auth/auth.module.ts
@@ -2,12 +2,19 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/modules/user/entities/user.entity';
 import { AuthController } from './auth.controller';
+import { AuthGuard } from './auth.guard';
 import { AuthService } from './auth.service';
 import { Auth } from './entities/auth.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, Auth])],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    {
+      provide: 'APP_GUARD',
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AuthModule {}
